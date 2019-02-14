d5a0ea05ff59c42368ff01b3c94421e94e7888b4
huangjing <1161172747@qq.com>
2019-02-14 16:24:21 +0800

    登录功能开发完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/home'
     }
+  
   ]
 })
+
+router.beforeEach((to,from,next)=> {
+    if (to.path === '/login') return next();
+
+    const tokenStr = window.sessionStorage.getItem('token');
+
+    if (!tokenStr) return this.a.push('/login')
+
+    next()
+})
+
+export default router
